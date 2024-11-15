daf7466052bd44c9e6597ba86a767f894fb29fdd
Sarthak100794 <74720416+Sarthak100794@users.noreply.github.com>
2024-11-15 13:22:21 +0530

    Update main.tf

--- assignment/main.tf
+++ assignment/main.tf
@@ -1,31 +1,31 @@
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.main.token
-}
+#provider "kubernetes" {
+#  host                   = module.eks.cluster_endpoint
+#  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#  token                  = data.aws_eks_cluster_auth.main.token
+#}
 
-data "aws_eks_cluster_auth" "main" {
-  name = module.eks.cluster_name
-}
+#data "aws_eks_cluster_auth" "main" {
+#  name = module.eks.cluster_name
+#}
  
-  module "vpc" {
-    source = "./modules/vpc"
-    cidr_block = var.vpc_cidr
-    private_subnets = var.private_subnets
-    public_subnets = var.public_subnets
- }
+#  module "vpc" {
+#    source = "./modules/vpc"
+#    cidr_block = var.vpc_cidr
+#    private_subnets = var.private_subnets
+#    public_subnets = var.public_subnets
+# }
 
- module "eks" {
-   source = "./modules/eks"
-   cluster_name = var.cluster_name
-   cluster_version = var.cluster_version
-   vpc_id = module.vpc.vpc_id
-   subnet_ids = module.vpc.private_subnets
-   node_groups = var.node_groups
-   ksa_name    = var.ksa_name
-   deployments  = var.deployments
- }
+# module "eks" {
+#   source = "./modules/eks"
+#   cluster_name = var.cluster_name
+#   cluster_version = var.cluster_version
+#   vpc_id = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+#   node_groups = var.node_groups
+#   ksa_name    = var.ksa_name
+#   deployments  = var.deployments
+# }
 
 module "s3" {
   source = "./modules/s3"
