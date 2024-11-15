f72bd86614c7bd770c507d4b676f656849e926bc
Sarthak100794 <74720416+Sarthak100794@users.noreply.github.com>
2024-11-15 14:41:40 +0530

    Update main.tf

--- assignment/main.tf
+++ assignment/main.tf
@@ -27,9 +27,13 @@
 #   deployments  = var.deployments
 # }
 
-module "s3" {
-  source = "./modules/s3"
-  bucket_name = var.s3_bucket_name
+resource "aws_s3_bucket" "main" {
+  bucket = "gsc-plugin-postsales"
+
+lifecycle {
+    ignore_changes = all
+    
+  }
 }
 
 
